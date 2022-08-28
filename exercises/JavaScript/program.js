227711a977e40731ad301c39b9a6a6295857778b
EnocCa <carranzaenoc@gmail.com>
2022-08-28 14:28:38 -0500

    7 and 8

--- exercises/JavaScript/program.js
+++ exercises/JavaScript/program.js
@@ -1,10 +1,14 @@
 var body;
 
 // implement onload function
-
-
+window.onload = function() {
+    body = document.querySelector("body");
+}
 // implement changeColorBlue
-
-
+window.changeColorBlue = function() {
+    body.style.backgroundColor = "#a8ceff";
+}
 // implement changeColorPurple
-
+window.changeColorRed = function() {
+    body.style.backgroundColor = "#ff0000";
+}
