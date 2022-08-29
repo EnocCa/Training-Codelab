65d531667c2490de0593f52f8b3db7678b1c5fe2
EnocCa <carranzaenoc@gmail.com>
2022-08-29 12:02:43 -0500

    currencybutton

--- frontend/src/Components/CurrencyButton.js
+++ frontend/src/Components/CurrencyButton.js
@@ -13,8 +13,16 @@ import styles from "./CurrencyButton.module.css"
 function CurrencyButton ({currency,changeCurrency}) {
   // ToDo 10.1
   return (
-    <>
-    </>     
+<div className={styles.bodyContainer}>
+
+      <button onClick={()=>changeCurrency("USD")}
+          className={currency === 'USD' ? styles.currencyButtonActive : styles.currencyButtonDefault} 
+          /*in the value */> USD </button>
+      <button onClick={()=>changeCurrency("MXN")}
+          className={currency === 'MXN' ? styles.currencyButtonActive : styles.currencyButtonDefault} 
+          /*in the value */> MXN </button>
+    </div>        
+
   );
 
 }
